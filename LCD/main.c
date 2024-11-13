0f4ab03546de050790814a0e920cfd7b9d890c47
IgnacioChurata <ignaciojavierchurataalzu@itisjt.edu.ar>
2024-11-13 17:07:16 -0300

    Create main.c

---
+++ LCD/main.c
@@ -0,0 +1,10 @@
+#include "LCD_I2C.h"
+#include "I2C.h"
+#include "MKL25Z4.h"
+int main (){
+    while (1){
+        LCD_Print(char * data);
+        printf(LCD_Print(char * data));
+    }
+    return 0;
+}
